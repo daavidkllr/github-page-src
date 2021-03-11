3028ab2196f7538ee2083012de45857b7b5bb435
David Keller <daavidkllr@outlook.de>
2021-03-12 00:00:55 +0100

    redesign / added about me section / prepared other sections

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import "./scss/index.scss";
 
+// ############################################################
+//  1. Hero Teaser: Particle background
+// ############################################################
 import "particles.js";
-particlesJS.load('background-particles', 'assets/particles.json', function() {
+
+particlesJS.load('background-particles', 'assets/particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
 
+// ############################################################
+// 2. Hero Teaser: Typewriter
+// ############################################################
 import Typed from 'typed.js';
+
 new Typed(
 	'.hero-container__hero.animate__typed',
 	{
@@ -13,7 +21,7 @@ new Typed(
 		smartBackspace: true,
 		typeSpeed: 75,
 		backSpeed: 75,
-		startDelay: 3150,
+		startDelay: 3000,
 		backDelay: 3000,
 		loop: false,
 		showCursor: true,
@@ -23,6 +31,9 @@ new Typed(
 	}
 );
 
+// ############################################################
+// 3. Hero Teaser: Button to Scroll down to the next section
+// ############################################################
 document.getElementById('scroll-down-button').addEventListener('click', () => {
 	const anchor = document.querySelector('#aboutme-main-container');
 	anchor.scrollIntoView({
